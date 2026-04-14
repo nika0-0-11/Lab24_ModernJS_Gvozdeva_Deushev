c6dd5839497aa839a9ce082ffde6e98618577de6
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-14 15:25:48 +0300

    spread/rest in JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-const { use } = require("react");
-
-console.log("\n=== Деструктуризация массивов ===");
+// console.log("\n=== Деструктуризация массивов ===");
 
 // const colors = ["red", "green", "blue"];
 // const color1 = colors[0];
@@ -16,7 +14,7 @@ console.log("\n=== Деструктуризация массивов ===");
 // const [c1, c2, c3, c4 = "yellow"] = colors;
 // console.log(c4);
 
-console.log("Деструктуризация объектов");
+// console.log("Деструктуризация объектов");
 
 // const user = {
 //     name: "Nika",
@@ -37,7 +35,7 @@ console.log("Деструктуризация объектов");
 // const { name: personName, country = "Россия" } = user;
 // console.log(personName, country);
 
-console.log("Деструктуризация в параметрах");
+// console.log("Деструктуризация в параметрах");
 
 // function printUserOld(user) {
 //     console.log(`Имя: ${user.name}`);
@@ -57,24 +55,104 @@ console.log("Деструктуризация в параметрах");
 
 // 6.5. Практическое задание
 
-const product = {
-  name: "Lego",
-  price: 6767,
-  category: "Toys",
-  inStock: true,
-};
+// const product = {
+//   name: "Lego",
+//   price: 6767,
+//   category: "Toys",
+//   inStock: true,
+// };
 
-const productName = product.name;
-const productPrice = product.price;
-const productCategory = product.category;
-const productInStock = product.inStock;
-console.log(productName, productPrice, productCategory, productInStock);
+// const productName = product.name;
+// const productPrice = product.price;
+// const productCategory = product.category;
+// const productInStock = product.inStock;
+// console.log(productName, productPrice, productCategory, productInStock);
 
-function printProduct({ name, price, category, inStock }) {
-  console.log(`Название: ${name}`);
-  console.log(`Цена: ${price}`);
-  console.log(`Категория: ${category}`);
-  console.log(`В наличии: ${inStock}`);
+// function printProduct({ name, price, category, inStock }) {
+//   console.log(`Название: ${name}`);
+//   console.log(`Цена: ${price}`);
+//   console.log(`Категория: ${category}`);
+//   console.log(`В наличии: ${inStock}`);
+// }
+
+// printProduct(product);
+
+// console.log("Spread для массивов");
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+
+// const combined = [...arr1, ...arr2];
+// console.log("Объединенный массив:", combined);
+
+// const copy = [...arr1];
+// console.log("Копия массива:", copy);
+
+// const extended = [0, ...arr1, 7, 8];
+// console.log("Расширенный массив:", extended);
+
+// console.log("Spread для объектов");
+
+// const person = {
+//     name: "Timur",
+//     age: 18,
+// };
+
+// const address = {
+//     city: "Санкт-Петербург",
+//     street: "Невский проспект",
+// };
+
+// const fullInfo = { ...person, ...address};
+// console.log("Полная информация:", fullInfo);
+
+// const personCopy = { ...person };
+// console.log("Копия объекта:", personCopy);
+
+// const updated = { ...person, age: 31, occupation: "Developer"};
+// console.log("Обновленный объект:", updated);
+
+// console.log("Rest оператор");
+
+// function sum(...numbers) {
+//     return numbers.reduce((total, num) => total + num, 0);
+// }
+
+// console.log("Сумма 1, 2, 3:", sum(1, 2, 3));
+// console.log("Сумма 1, 2, 3, 4, 5:", sum(1, 2, 3, 4, 5));
+
+// const numbers = [10, 20, 30, 40, 50];
+// const [first, second, ...rest] = numbers;
+
+// console.log("Первое число:", first);
+// console.log("Второе число:", second);
+// console.log("Остальные числа:", rest);\
+
+
+// 7.5. Практическое задание
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
+
+const arrAll = [...arr1, ...arr2];
+console.log("Объединенны массив:", arrAll);
+
+function findMax(...numbers) {
+    return Math.max(...numbers);
 }
 
-printProduct(product);
+console.log(findMax(...arrAll));
+
+
+const obj1 = {
+    name: "Nika",
+    age: 18,
+};
+
+const obj2 = {
+    city: "Волжкий",
+    street: "40 лет победы",
+};
+
+const objAll = { ...obj1, ...obj2};
+console.log(objAll);
